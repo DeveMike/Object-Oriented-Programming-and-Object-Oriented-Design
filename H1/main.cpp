3e5759ea3e1a93cf034702d3d521f0b2741dd75b
Mihail Rulinskiy <roulinski@hotmail.com>
2024-10-30 19:39:39 +0200

    h1 valmis

--- H1/main.cpp
+++ H1/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
+#include "myfunctions.h"
 using namespace std;
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int num;
+    int result;
+    cout <<"LASKETAAN kertoma!" << endl;
+    cout << "anna pos luku" << endl;
+
+    cin >> num;
+    result=factorial(num);
+    if(result==-1){
+    cout << "Kertomaa ei voi laskea negatiiviselle luvulle" << endl;
+    } else {
+     cout << "Luvun " << num << " kertoma = " << factorial(num) << endl;
+
+        }
     return 0;
 }
